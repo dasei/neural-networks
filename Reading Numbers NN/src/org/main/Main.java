c7424001d473ea4fba219b179622b63425e0ed17
Daniel <daniel.seidel.work@gmail.com>
2018-02-16 19:23:39 +0100

    added getReasonWhyDanielIsSuchAGreatMan Method

--- Reading Numbers NN/src/org/main/Main.java
+++ Reading Numbers NN/src/org/main/Main.java
@@ -4,4 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("funzt?!!");
 	}
+
+	public static void getReasonWhyDanielIsSuchAGreatMan(int yourCreditCardID) {
+
+	}
 }
