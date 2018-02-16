693dd87019a58dda3a04572abb10ec2b80a24c80
Marco Wiedmann <marco_wiedmann@web.de>
2018-02-16 19:24:23 +0100

    void virusStarten

--- Reading Numbers NN/src/org/main/Main.java
+++ Reading Numbers NN/src/org/main/Main.java
@@ -1,7 +1,13 @@
 package org.main;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		System.out.println("funzt?!!");
 	}
+	
+	public void virusStarten(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+	
 }
